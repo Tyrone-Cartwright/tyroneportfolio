449c7b09db3db3d018f0562a2dbf6b3f5d87e53c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2022-01-07 11:03:15 -0500

    minor fix to port

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Portfolio site listening on ${port}`);
+  console.log(`Portfolio site is listening on ${port}`);
 });
